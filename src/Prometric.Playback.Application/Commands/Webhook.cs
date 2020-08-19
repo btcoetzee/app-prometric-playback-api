41fb630f05db1b541761ef9d07a4f94fa1ad567d
Chris Rictor <chris.rictor@contractor.prometric.com>
2020-08-19 18:02:35 -0400

    example of pushing key value pairs into commands

---
+++ src/Prometric.Playback.Application/Commands/Webhook.cs
@@ -0,0 +1,17 @@
+﻿using Convey.CQRS.Commands;
+
+namespace Prometric.Playback.Application.Commands
+{
+    public class Webhook : ICommand
+    {
+        public string RoomName { get; set; }
+        public string RoomStatus { get; set; }
+        public string RoomType { get; set; }
+        public string RoomSid { get; set; }
+        public string RoomDuration { get; set; }
+        public string SequenceNumber { get; set; }
+        public string StatusCallbackEvent { get; set; }
+        public string Timestamp { get; set; }
+        public string AccountSid { get; set; }
+    }
+}
