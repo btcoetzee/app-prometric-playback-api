8dfac58d73e467caa34f68f2b7f71059d39463e3
Justin Baldwin <Justin.Baldwin@contractor.prometric.com>
2020-08-19 21:44:53 -0400

    Working functionality of recording complete webhook, initialized composition webhook

--- src/Prometric.Playback.Application/Commands/AddComposition.cs
+++ src/Prometric.Playback.Application/Commands/AddComposition.cs
@@ -6,15 +6,19 @@ namespace Prometric.Playback.Application.Commands
 {
     public class AddComposition : ICommand
     {
-        public AddComposition(Guid compositionId, string compositionName)
-        {
-            CompositionId = compositionId == Guid.Empty ? Guid.NewGuid() : compositionId;
-            CompositionName = compositionName;
-        }
-
-        public Guid CompositionId { get; set; }
-
-        [Required]
-        public string CompositionName { get; set; }
+        public string AccountSid {get; set;}
+        public string RoomSid {get; set;}
+        public string HookSid {get; set;}
+        public string HookUri {get; set;}
+        public string HookFriendlyName {get; set;}
+        public string CompositionSid {get; set;}
+        public string CompositionUri {get; set;}
+        public string MediaUri {get; set;}
+        public string Duration {get; set;}
+        public string Size {get; set;}
+        public string PercentageDone {get; set;}
+        public string SecondsRemaining {get; set;}
+        public string StatusCallbackEvent {get; set;}
+        public System.DateTime Timestamp {get; set;}
     }
 }
--- src/Prometric.Playback.Application/Commands/Handlers/AddRecordingHandler.cs
+++ src/Prometric.Playback.Application/Commands/Handlers/AddRecordingHandler.cs
@@ -1,13 +1,15 @@
 ﻿using Convey.CQRS.Commands;
 using Microsoft.Extensions.Logging;
-using Prometric.Playback.Application.Exceptions;
-using Prometric.Playback.Core.Entities;
 using Prometric.Playback.Core.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Twilio;
-using Twilio.Rest.Video.V1.Room;
+using Twilio.Rest.Video.V1;
+using System.Linq;
+using static Twilio.Rest.Video.V1.CompositionResource;
+using Microsoft.Extensions.Configuration;
+using ProProctor.Conferences.Infrastructure.Services;
 
 namespace Prometric.Playback.Application.Commands.Handlers
 {
@@ -15,23 +17,29 @@ namespace Prometric.Playback.Application.Commands.Handlers
     {
         private readonly IRecordingRepository _repository;
         private readonly ILogger<AddRecordingHandler> _log;
-        public AddRecordingHandler(IRecordingRepository repository, ILogger<AddRecordingHandler> log) { 
+        public AddRecordingHandler(IRecordingRepository repository, ILogger<AddRecordingHandler> log, IConfiguration config) { 
+            TwilioClientConfig.InitTwilioClient(config);
             _repository = repository;
             _log = log;
         }
 
         public async Task HandleAsync(AddRecording command)
         {
-            // TODO: move to environment variables
-            TwilioClient.Init("ACCT_SID", "AUTH_TOKEN");
-            if (command.RecordingStatusCallback.StatusCallbackEvent == "recording-completed" && command.RecordingStatusCallback.ParticipantIdentity == "candidate") {
-                // Fetch all recordings for the given participant and room sid
-                var recordings = await RoomRecordingResource.ReadAsync(pathRoomSid: command.RecordingStatusCallback.RoomSid, sourceSid: command.RecordingStatusCallback.ParticipantSid);
+            var groupingSid = new List<string> {
+                command.RoomSid, command.ParticipantSid
+            };
 
-                foreach(var record in recordings) {
-                    Console.WriteLine(record.Sid);
-                }
-            }
+            // Fetch relevant recordings for room, participant, and after the current timestamp (adjusted for latency)
+            var recordings = await RecordingResource.ReadAsync(groupingSid: groupingSid, dateCreatedAfter: command.Timestamp.AddMinutes(-1));
+            // Post a new Composition to the Twilio API, use our composition callback
+            var layout = new { single = new { video_sources = recordings.Where(record => record.Type.ToString() == "video").Select(record => record.Sid).ToList() }};
+            var composition = await CompositionResource.CreateAsync(
+                roomSid: command.RoomSid,
+                audioSources: recordings.Where(record => record.Type.ToString() == "audio").Select(record => record.Sid).ToList(),
+                videoLayout: layout,
+                statusCallback: new Uri("https://01d7c01c207a.ngrok.io/compositions"),
+                format: FormatEnum.Mp4
+            );
         }
     }
 }
